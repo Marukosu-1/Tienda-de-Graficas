6a60ee8e1b997cc916ecb7f1bd35df9602a228c3
Marukosu-1 <xdm4163@gmail.com>
2024-07-16 14:59:12 -0500

    Agregando los archivos de la tienda de graficas

---
+++ html/js/contactos.js
@@ -0,0 +1,16 @@
+
+function limpiarDato() { 
+
+    document.getElementById('nombre').value = '';
+    document.getElementById('correo').value = '';
+    document.getElementById('telefono').value= '';
+    document.getElementById('text-area').value = '';
+}
+
+
+
+function enviarDato(){
+
+    limpiarDato();
+    
+}
